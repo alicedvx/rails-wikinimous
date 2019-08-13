3baf058aa0a928b9c5232993c2bb4e69471c007e
Alice Deveaux <alice.dvx@icloud.com>
2019-08-13 16:54:28 +0200

    done without style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
+
+# 7 CRUD default actions to
+# list, show, create, update and destroy an article
